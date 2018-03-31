399da4420459d30a8c7a89d310c124199eaf6b6e
Yuya TAMANO <everfree.main@gmail.com>
2018-03-31 17:47:11 +0900

    Retrieve file list

--- lib/slack_file_cleaner.ex
+++ lib/slack_file_cleaner.ex
@@ -1,18 +1,53 @@
 defmodule SlackFileCleaner do
   @moduledoc """
-  Documentation for SlackFileCleaner.
+  Remove all files uploaded to Slack
   """
 
-  @doc """
-  Hello world.
+  def main(args) do
+    {options, _, _} = OptionParser.parse(
+      args,
+      switches: [token: :string, before: :integer],
+      aliases: [t: :token, b: :before]
+    )
 
-  ## Examples
+    Enum.each(target_file_ids(options), &(IO.puts(&1)) )
+  end
 
-      iex> SlackFileCleaner.hello
-      :world
+  defp target_file_ids(options) do
+    get_max_page(options)
+    |> list_file_ids(options, [])
+  end
 
-  """
-  def hello do
-    :world
+  defp get_max_page(options) do
+    fetch_file_list(options)
+    |> json_paging_max
+  end
+
+  defp list_file_ids(0, _, ids), do: ids
+  defp list_file_ids(page, options, ids) do
+    IO.puts("retriving page: #{page}")
+
+    new_ids = fetch_file_list(options, page)
+              |> json_files
+              |> file_ids([])
+
+    list_file_ids(page-1, options, new_ids ++ ids)
+  end
+
+  defp fetch_file_list(options, page \\ 1) do
+    "https://slack.com/api/files.list?token=#{options[:token]}&ts_to=#{options[:before]}&page=#{page}"
+    |> HTTPoison.get!
+    |> http_body
+    |> Poison.decode!
+  end
+
+  defp http_body( %{ status_code: 200, body: json} ), do: json
+  defp json_paging_max( %{ "ok" => true, "paging" => %{"pages" => max} } ), do: max
+  defp json_files( %{ "ok" => true, "files" => body } ), do: body
+
+  defp file_ids([], ids), do: ids
+  defp file_ids([ head | tail],  ids) do
+    %{ "id" => file_id } = head
+    file_ids(tail, [file_id] ++ ids)
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule SlackFileCleaner.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(), 
+      escript: [main_module: SlackFileCleaner]
     ]
   end
 
@@ -21,8 +22,8 @@ defmodule SlackFileCleaner.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 0.6"}, 
+      {:poison, "~> 3.1"}, 
     ]
   end
 end
