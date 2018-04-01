c51c60c6de00f254ff9c252dd8523c54cd091178
Yuya TAMANO <everfree.main@gmail.com>
2018-04-01 22:05:35 +0900

    Implement deleting files

--- lib/slack_file_cleaner.ex
+++ lib/slack_file_cleaner.ex
@@ -6,11 +6,23 @@ defmodule SlackFileCleaner do
   def main(args) do
     {options, _, _} = OptionParser.parse(
       args,
-      switches: [token: :string, before: :integer],
+      switches: [token: :string, before: :integer, dryrun: :boolean],
       aliases: [t: :token, b: :before]
     )
 
-    Enum.each(target_file_ids(options), &(IO.puts(&1)) )
+    IO.puts("** Start searching target files")
+
+    target_file_ids(options)
+    |> fn(f) ->
+        # FIXME: maybe this is not a good way
+        IO.puts("** Start deleting #{Enum.count(f)} files #{if options[:dryrun] == true, do: "(dry run)"}")
+        f
+      end.()
+    |> Enum.map( fn(file_id) ->
+      delete_file(options, file_id)
+      # didn't make it parallel to prevent from hitting API limit.
+      # spawn(fn -> delete_file(options, file_id) end)
+      end)
   end
 
   defp target_file_ids(options) do
@@ -44,13 +56,30 @@ defmodule SlackFileCleaner do
     |> Poison.decode!
   end
 
+  defp delete_file(options, file_id) do
+    if options[:dryrun] do
+      IO.puts("Deleting file: #{file_id} (dry run)")
+    else
+      "https://slack.com/api/files.delete?token=#{options[:token]}&file=#{file_id}"
+      |> HTTPoison.post!([])
+      |> http_body
+      |> Poison.decode!
+      |> json_delete_result
+      |> fn(result) -> IO.puts("Deleting file: #{file_id} (#{result})") end.()
+    end
+  end
+
   defp http_body( %{ status_code: 200, body: json} ), do: json
+  defp http_body( %{ status_code: 429, body: json} ), do: json   # rate limit
   defp json_paging_max( %{ "ok" => true, "paging" => %{"pages" => max} } ), do: max
   defp json_files( %{ "ok" => true, "files" => body } ), do: body
+  defp json_delete_result( %{ "ok" => true } ), do: "success"
+  defp json_delete_result( %{ "ok" => false, "error" => body} ), do: "error: #{body}"
 
   defp file_ids([], ids), do: ids
   defp file_ids([ head | tail],  ids) do
     %{ "id" => file_id } = head
     file_ids(tail, [file_id] ++ ids)
   end
+
 end
